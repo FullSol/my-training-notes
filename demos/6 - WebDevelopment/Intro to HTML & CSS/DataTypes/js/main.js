27c66ab93d6fc88044000417ae64becb39265696
FullSol <ceraines@student.fullsail.edu>
2022-05-24 13:17:08 -0400

    incomplete, but meh

---
+++ demos/6 - WebDevelopment/Intro to HTML & CSS/DataTypes/js/main.js
@@ -0,0 +1,7 @@
+// 1. declare variables for each type
+var num = 10;
+var str = num + ' is my favorite number.';
+var nullData = null;
+var undefinedData = '';
+
+// Symbols allow us to create "hidden" properties of an object, that no other part of code
